5f1467f23b4dfd921959a1428eef1b4b0ae6cc3f
Yonga Edouard <yonga@wazahub.com>
2020-04-13 16:02:02 +0100

    test

--- src/estimator.js
+++ src/estimator.js
@@ -1,12 +1,18 @@
 const covid19ImpactEstimator = (data) => {
 
 	const input = data
+	const currentlyInfected = reportedCases * 10
+	const currentlyInfected = reportedCases * 50
 
 	return {
-		data:,
-		impact:{},
-		severeImpact: {}
+		"data": input,
+		"impact":{
+			"currentlyInfected": currentlyInfected,
+		},
+		"severeImpact": {
+			"currentlyInfected": currentlyInfected,
+		}
 	};
-}
+};
 
 export default covid19ImpactEstimator;
