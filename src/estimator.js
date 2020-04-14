114beb48e41d9582dce9ae5be3f21f36b02a499b
Yonga Edouard <yonga@wazahub.com>
2020-04-14 14:36:58 +0100

    test cha1

--- src/estimator.js
+++ src/estimator.js
@@ -41,63 +41,63 @@ const covid19ImpactEstimator = (data) => {
     impact.infectionsByRequestTime = impact.currentlyInfected * (2 ** timeFactor);
     severeImpact.infectionsByRequestTime = severImpact.currentlyInfected * (2 ** timeFactor);
 
-    //challenge 2
+    // //challenge 2
 
-    const impactRequestedTime = impact.infectionsByRequestedTime * 0.15;
-    const severeImpactRequest = severeImpact.infectionsByRequestedTime * 0.15;
+    // const impactRequestedTime = impact.infectionsByRequestedTime * 0.15;
+    // const severeImpactRequest = severeImpact.infectionsByRequestedTime * 0.15;
 
-    impact.severeCasesByRequestedTime = Math.trunc(impactRequestedTime);
-    severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpactRequest);
+    // impact.severeCasesByRequestedTime = Math.trunc(impactRequestedTime);
+    // severeImpact.severeCasesByRequestedTime = Math.trunc(severeImpactRequest);
 
-    const bedsAvailable = totalHospitalBeds * 0.35;
-    const ImpactHospitlBedval = bedsAvailable - impactRequestedTime;
-    const sevImpactHospitalBedval = bedsAvailable - severeImpactRequest;
+    // const bedsAvailable = totalHospitalBeds * 0.35;
+    // const ImpactHospitlBedval = bedsAvailable - impactRequestedTime;
+    // const sevImpactHospitalBedval = bedsAvailable - severeImpactRequest;
 
-    impact.hospitalBedsByRequestedTime = Math.trunc(sevImpactHospitalBedval);
+    // impact.hospitalBedsByRequestedTime = Math.trunc(sevImpactHospitalBedval);
 
-    //challenge 3
+    // //challenge 3
 
-    const ImpactCasesforICU = impact.infectionsByRequestedTime * 0.05;
-    const sevImpactCasesforICE = severeImpact.infectionsByRequestedTime * 0.05;
-    const ImpactVentilator = impact.infectionsByRequestedTime *0.02;
-    const sevImpactVentillator = severeImpact.infectionsByRequestedTime * 0.02;
+    // const ImpactCasesforICU = impact.infectionsByRequestedTime * 0.05;
+    // const sevImpactCasesforICE = severeImpact.infectionsByRequestedTime * 0.05;
+    // const ImpactVentilator = impact.infectionsByRequestedTime *0.02;
+    // const sevImpactVentillator = severeImpact.infectionsByRequestedTime * 0.02;
 
 
-    impact.casesForICUByRequestedTime = Math.trunc(ImpactCasesforICU);
-    severeImpact.casesForICUByRequestedTime = Math.trunc(sevImpactCasesforICU);
+    // impact.casesForICUByRequestedTime = Math.trunc(ImpactCasesforICU);
+    // severeImpact.casesForICUByRequestedTime = Math.trunc(sevImpactCasesforICU);
 
-    impact.casesForVentilatorsByRequestedTime = Math.trunc(ImpactVentilator);
-    severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(sevImpactVentillator);
+    // impact.casesForVentilatorsByRequestedTime = Math.trunc(ImpactVentilator);
+    // severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(sevImpactVentillator);
 
-    let newDay;
-    const compute = population * avgDailyIncomeInUsd;
-    if (periodType === 'months'){
-        newDay = timeToElapse * 30;
+    // let newDay;
+    // const compute = population * avgDailyIncomeInUsd;
+    // if (periodType === 'months'){
+    //     newDay = timeToElapse * 30;
 
-        impact.dollarsInFlight = {
-            Math.trunc((impact.infectionsByRequestedTime * compute)/ newDay)
-        };
-        severeImpact.dollarsInFlight = {
-            Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
-        };
+    //     impact.dollarsInFlight = {
+    //         Math.trunc((impact.infectionsByRequestedTime * compute)/ newDay)
+    //     };
+    //     severeImpact.dollarsInFlight = {
+    //         Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
+    //     };
    
-    }else if (periodType === 'weeks'){
-        newDay = timeToElapse * 7;
-        impact.dollarsInFlight = {
-            Math.trunc((impact.infectionsByRequestedTime * compute) / newDay)
-        };
-        severeImpact.dollarsInFlight = {
-            Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
-        };
-    }else if (periodType === 'days'){
-        newDay = timeToElapse * 1;
-        impact.dollarsInFlight = {
-            Math.trunc((impact.infectionsByRequestedTime * compute) / newDay)
-        };
-        severeImpact.dollarsInFlight = {
-            Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
-        };
-    }
+    // }else if (periodType === 'weeks'){
+    //     newDay = timeToElapse * 7;
+    //     impact.dollarsInFlight = {
+    //         Math.trunc((impact.infectionsByRequestedTime * compute) / newDay)
+    //     };
+    //     severeImpact.dollarsInFlight = {
+    //         Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
+    //     };
+    // }else if (periodType === 'days'){
+    //     newDay = timeToElapse * 1;
+    //     impact.dollarsInFlight = {
+    //         Math.trunc((impact.infectionsByRequestedTime * compute) / newDay)
+    //     };
+    //     severeImpact.dollarsInFlight = {
+    //         Math.trunc((severeImpact.infectionsByRequestedTime * compute) / newDay)
+    //     };
+    // }
 
     return{     
         data,
