531e1f3a61d3eb40ef1128bb02a285f1c39a352a
Yonga Edouard <yonga@wazahub.com>
2020-04-14 16:12:44 +0100

    test cha1

--- src/estimator.js
+++ src/estimator.js
@@ -3,9 +3,9 @@ const covid19ImpactEstimator = (data) => {
     // region: {
     //   // avgDailyIncomeInUsd
     // },
-    reportedCases,
-    timeToElapse,
-    periodType
+    reportedCases
+    // timeToElapse,
+    // periodType
     // population,
     // totalHospitalBeds
 
@@ -22,25 +22,25 @@ const covid19ImpactEstimator = (data) => {
 
   let timeFactor;
 
-  switch (periodType.trim().toLowerCase()) {
-    case 'months':
-      timeFactor = Math.trunc((timeToElapse * 30) / 3);
-      break;
+  // switch (periodType.trim().toLowerCase()) {
+  //   case 'months':
+  //     timeFactor = Math.trunc((timeToElapse * 30) / 3);
+  //     break;
 
-    case 'weeks':
-      timeFactor = Math.trunc((timeToElapse * 7) / 3);
-      break;
+  //   case 'weeks':
+  //     timeFactor = Math.trunc((timeToElapse * 7) / 3);
+  //     break;
 
-    case 'days':
-      timeFactor = Math.trunc((timeToElapse) / 3);
-      break;
+  //   case 'days':
+  //     timeFactor = Math.trunc((timeToElapse) / 3);
+  //     break;
 
-    default:
-  }
+  //   default:
+  // }
 
   // time passed as infection rates grow
-  impact.infectionsByRequestTime = impact.currentlyInfected * (2 ** timeFactor);
-  severeImpact.infectionsByRequestTime = severeImpact.currentlyInfected * (2 ** timeFactor);
+  impact.infectionsByRequestTime = impact.currentlyInfected * 512;
+  severeImpact.infectionsByRequestTime = severeImpact.currentlyInfected * 512;
 
   // //challenge 2
 
